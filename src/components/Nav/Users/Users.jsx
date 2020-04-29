10411975f1f4e828c62621cb6ab8188e7e2a71a8
Yurii <haik.yu.rii@gmail.com>
2020-04-29 23:42:02 +0300

    added life cycled method

--- src/components/Nav/Users/Users.jsx
+++ src/components/Nav/Users/Users.jsx
@@ -2,35 +2,37 @@ import React from "react";
 import s from "./Users.module.scss"
 import * as axios from "axios";
 
-const Users = (props) => {
-    if (props.users.length ===  0){
+class Users extends React.Component{
 
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(
+            this.props.setUsers(
                 response.data.items
             )
         })
-
     }
 
+    render() {
+        return(
+            <div>
+                <div className={s.title}>
+                    {
+                        this.props.users.map( u => <div key={u.id}>
+                            <div>{u.name}</div>
+                            <div>
+                                {u.followed
+                                    ? <button onClick={ () => {this.props.unfollow(u.id)}}>Unfollow</button>
+                                    : <button onClick={ () => {this.props.follow(u.id)}}>Follow</button>
+                                }
+                            </div>
+                        </div>)
 
+                    }
+                </div>
+            </div>
 
-    return(
-        <div className={s.title}>
-            {
-                props.users.map( u => <div key={u.id}>
-                    <div>{u.name}</div>
-                    <div>
-                        {u.followed
-                            ? <button onClick={ () => {props.unfollow(u.id)}}>Unfollow</button>
-                            : <button onClick={ () => {props.follow(u.id)}}>Follow</button>
-                        }
-                    </div>
-                </div>)
-
-            }
-        </div>
-    )
-};
+        )
+    }
+}
 
 export default Users;
